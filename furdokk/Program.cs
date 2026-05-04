d0db6f68c0c22e0a9d44b4693fe6b22c2c8bc53a
barditp <barditp@310-11>
2026-05-04 14:10:59 +0200

    javitas

--- furdokk/Program.cs
+++ furdokk/Program.cs
@@ -7,21 +7,21 @@ using System.Threading.Tasks;
 
 namespace StrandC
 {
-    public class Furdo {
+    public class Furdo
+    {
         public String név { get; private set; }
         public string cím { get; private set; }
         public int ár { get; private set; }
         public int vizhofok { get; private set; }
-    }
     public Furdo(string sor)
         {
             string[] adatok = sor.Split(";");
-            név=adatok[0];
+            név = adatok[0];
             cím = adatok[1];
             ár = int.Parse(adatok[2]);
             vizhofok = int.Parse(adatok[3]);
 
-        }
+        } 
     internal class Program
     {
         static void Main(string[] args)
