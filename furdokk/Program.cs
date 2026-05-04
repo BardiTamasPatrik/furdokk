135e313ac63c7d83e26158972327fbd7e6b3a986
barditp <barditp@310-11>
2026-05-04 14:43:15 +0200

    Lista hozza adasa

--- furdokk/Program.cs
+++ furdokk/Program.cs
@@ -34,7 +34,11 @@ namespace StrandC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        } 
+                List<Furdo> list = new List<Furdo>();
+                string[] sorok = File.ReadAllLines("strandadatok.txt").Skip(1).ToArray();
+                foreach (string s in sorok)
+                {
+                    list.Add(new Furdo(s));
+                } 
     }
 }
